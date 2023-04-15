5a4c213cfd970ec3b57bd353f95b66c30fc8911e
riddhiwho <sharmariddhi81@gmail.com>
2023-04-15 22:22:00 +0530

    traverse folders and subfolders entirely

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import hashlib
 import os 
 
-def hash_file(filepath):
-    os.chdir(filepath)
-    files = os.listdir(filepath)
-    
+def list_files(startpath):
     hash_dir = {}
-    
-    for filename in files:
-        h = hashlib.sha1()
-        hash_list = []
-        with open(filename,'rb') as file:
-            chunk = 0
-            while chunk != b'':
-                chunk = file.read(1024)
-                h.update(chunk)
-            hash_list.append(h.hexdigest())
-            if h.hexdigest() in hash_dir:
-                hash_dir[h.hexdigest()].append(filename)
-            else:
-                hash_dir[h.hexdigest()] = [filename]
+    for root, dirs, files in os.walk(startpath):
+        for file in files:
+            filename = (os.path.join(root, file))
+            # print(filename)
+            h = hashlib.sha1()
+            hash_list = []
+            with open(filename,'rb') as file:
+                chunk = 0
+                while chunk != b'':
+                    chunk = file.read(1024)
+                    h.update(chunk)
+                hash_list.append(h.hexdigest())
+                if h.hexdigest() in hash_dir:
+                    hash_dir[h.hexdigest()].append(filename)
+                else:
+                    hash_dir[h.hexdigest()] = [filename]
     return hash_dir
 
-filepath = input("Enter the file path: ")
-msg = hash_file(filepath)
-for key, value in msg.items():
-    if len(value) > 1:
-        print("Duplicate files found:", value)
+if __name__ == "__main__":
+    filepath = input("Enter the file path: ")
+    msg = list_files(filepath)
+    for key, value in msg.items():
+        if len(value) > 1:
+            print("Duplicate files found:", value)
